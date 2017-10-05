b4d72cf7d3494b782add42f145fc4b8fd1cc3fbd
xfmv12 <32549017+medcelerate@users.noreply.github.com>
2017-10-05 12:47:56 -0400

    test

---
+++ portals/stanford.js
@@ -0,0 +1,28 @@
+/*
+This is the parser for the stanford applicaiton, there needs to be a user agent in the user agent field.
+This specific package depends on horsemanjs and phantom js. Phantom can be installed using npm with -g.
+*/
+
+const Horseman = require('node-horseman');
+
+function accessStanford(aamcid, password) {
+    let horseman = new Horseman();
+    let userid = aamcid;
+    let pass = password;
+    let state = null;
+
+    horseman
+    .userAgent('')
+    .open('https://med.stanford.edu/aes')
+    .post('https://med.stanford.edu/aes/login.do','aamcId=&password=')
+    .wait(500)
+    .open('https://med.stanford.edu/aes/applicationStatus.do')
+    .html('table[class="application-table"]')
+    .then((text) => {
+        state = text;
+    })
+    .close()
+    .then(() => {
+        return state;
+    })
+}
