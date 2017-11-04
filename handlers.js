89883b41f34ad3c7190e4af27c0dfa96da5859cb
xfmv12 <32549017+medcelerate@users.noreply.github.com>
2017-11-04 14:13:02 -0400

    yes

--- handlers.js
+++ handlers.js
@@ -7,6 +7,7 @@ const mayo = require('./portals/mayo')
 const harvard = require('./portals/harvard')
 
 function computeDeltas(datau) {
+    if (fs.existsSync('./state.json')) {
         let state = {}
         let rawdata = fs.readFileSync('./state.json');  
         let data = JSON.parse(rawdata);
@@ -21,6 +22,21 @@ function computeDeltas(datau) {
                 }
             }
         }
+        fs.writeFileSync('./state.json', data)
+    }
+    else {
+        let state = {}
+        for (var key in datau) {
+            if (datau.hasOwnProperty(key)) {
+                state[key] = {data: datau, update: false};
+            }
+            else {
+                continue;
+            }
+        }
+        fs.writeFileSync('./state.json', state)
+    }
+        
 }
 
  async function pulldata(req) {
@@ -31,56 +47,6 @@ function computeDeltas(datau) {
     dataUpdate.ce = await casew.accessCase(req.body.email, req.body.pass);
     return dataUpdate
 }
-
-/*var performScrape = new Promise(
-    function scrape(resolve, reject) {
-        let state = {}
-        let rawdata = fs.readFileSync('./state.json');  
-        let data = JSON.parse(rawdata);
-        let datau = {};
-        stanford.accessStanford(request.body.aamcid, request.body.pass, (response) => {
-            datau.sd = response;
-            if (datau.sd != data.sd) {
-            data.sd = datau.sd
-            state.sd = true
-            }
-            else {
-                state.sd = false
-            }
-        });
-        casew.accessCase(request.body.email, request.body.pass, (response) => {
-            data.cw = response;
-            if (datau.case != data.case) {
-                data.case = datau.case
-                state.case = true
-            }
-            else {
-                state.case = false
-            }
-
-        });
-        harvard.accessHarvard(request.body.aamcid, request.body.pass, (response) => {
-            data.hd = response;
-            if (datau.hd != data.hd) {
-                data.hd = datau.hd
-                state.hd = true
-            }
-            else {
-                state.hd = false
-            }
-        });
-        mayo.accessMayo(request.body.aamcid, request.body.pass, (response) => {
-            data.mo = response;
-            if (datau.mo != data.mo) {
-                data.mo = datau.mo
-                state.mo = true
-            }
-            else {
-                state.mo = false
-            }
-            //resolve(state)
-        });
-})*/
 
 function pulldown(req, res, next) {
     pulldata(req)
